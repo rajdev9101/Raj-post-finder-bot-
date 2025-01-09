a87a965ca891527316103d3ec0c06e88d5388e09
rajdev9101 <devraj93957@gmail.com>
2025-01-09 16:50:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "27084955"))
+API_HASH     = environ.get("API_HASH", "91c88b554ab2a34f8b0c72228f06fc0b")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
-LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", ""))
-ADMIN        = int(environ.get("ADMIN", ""))
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ankushdeb91:WpbkXkwE0rL98h7q@cluster0.cpl3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002485759251"))
+ADMIN        = int(environ.get("ADMIN", "5804953849"))
 CHANNEL      = environ.get("CHANNEL", "@raj_dev_01")
